98ed2575922ee6023f3264a1d44eeb6b29d66846
bneayoub <bne.ayoub@gmail.com>
2024-06-27 06:24:00 +0100

    chore: Update Next.js configuration for lazy loading images and fetch popular movies from TMDB API
    Testing API (May change later)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
+import { fetchFromTMDB } from '@/utils/tmdb';
+import MovieList from '@/components/MovieList';
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetchFromTMDB({ path: '/movie/popular' });
+  const movies = data.results;
+
   return (
-   <h1>CineVault</h1>
+    <main className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Popular Movies</h1>
+      <MovieList movies={movies} />
+    </main>
   );
 }
