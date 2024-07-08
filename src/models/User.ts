a99a039c78c87a6cf235a9cf21208ce2d41fc550
bneayoub <bne.ayoub@gmail.com>
2024-07-08 07:08:46 +0100

    feat: Update User model to make email and name fields optional
    This commit updates the User model by making the email and name fields optional. This change allows for more flexibility in user data entry, as not all users may have an email or name associated with their account.

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
+  email: { type: String, required: false },
+  name: { type: String, required: false },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
