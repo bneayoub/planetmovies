d288d9ccff3bfc8f19b5b04b6613774d74c47544
bneayoub <bne.ayoub@gmail.com>
2024-07-08 06:44:21 +0100

    feat: Add MongoDB connection utility
    This commit adds the `mongodb.ts` file, which contains the utility function `dbConnect` for establishing a connection to MongoDB using Mongoose. The function checks if a connection already exists and returns it if available. Otherwise, it creates a new connection using the provided MongoDB URI. This utility enhances the application by providing a centralized and efficient way to connect to the database.

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+
+type CachedMongoose = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+declare global {
+  var mongoose: CachedMongoose | undefined;
+}
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+}
+
+const MONGODB_URI: string = process.env.MONGODB_URI;
+
+let cached: CachedMongoose = global.mongoose ?? {
+  conn: null,
+  promise: null,
+};
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
+async function dbConnect(): Promise<typeof mongoose> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default dbConnect;
