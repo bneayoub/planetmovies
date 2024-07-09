ef40f8197477d2735ac68bf2ff8447fe7e6cd3d3
bneayoub <bne.ayoub@gmail.com>
2024-07-09 08:43:22 +0100

    feat: Update tailwind.config.ts to include dark mode and custom skin colors
    This commit updates the tailwind.config.ts file to include the darkMode property set to 'class', enabling dark mode support for the application. It also adds custom skin colors for background and text elements, allowing for a more personalized and visually appealing user interface. These changes enhance the overall user experience and provide more flexibility in styling the application.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,26 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      backgroundColor: {
+        skin: {
+          fill: 'var(--color-fill)',
+          'button-accent': 'var(--color-button-accent)',
+          'button-accent-hover': 'var(--color-button-accent-hover)',
+        },
+      },
+      textColor: {
+        skin: {
+          base: 'var(--color-text-base)',
+          muted: 'var(--color-text-muted)',
+          inverted: 'var(--color-text-inverted)',
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
