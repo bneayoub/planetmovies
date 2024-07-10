b215d4da79fd85308f70105f2e2d3485b775d991
bneayoub <bne.ayoub@gmail.com>
2024-07-10 20:07:12 +0100

    refactor: Update movie and TV show routes to fetch similar content
    Update the movie and TV show routes to fetch similar content from the TMDB API. This refactor improves the functionality of the application by providing users with recommendations for similar movies and TV shows based on their selected content.

--- src/app/api/movie/[id]/route.ts
+++ src/app/api/movie/[id]/route.ts
@@ -4,11 +4,16 @@ import { fetchFromTMDB } from '@/utils/tmdb';
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   const id = params.id;
   try {
-    const data = await fetchFromTMDB({
-      path: `/movie/${id}`,
-      params: { append_to_response: 'videos,credits' },
-    });
-    return NextResponse.json(data);
+    const [movieDetails, similarMovies] = await Promise.all([
+      fetchFromTMDB({
+        path: `/movie/${id}`,
+        params: { append_to_response: 'videos,credits' },
+      }),
+      fetchFromTMDB({
+        path: `/movie/${id}/similar`,
+      }),
+    ]);
+    return NextResponse.json({ ...movieDetails, similarMovies: similarMovies.results });
   } catch (error) {
     console.error(`Error fetching movie details for id ${id}:`, error);
     return NextResponse.json({ error: 'Failed to fetch movie details' }, { status: 500 });
--- src/app/api/tv/[id]/route.ts
+++ src/app/api/tv/[id]/route.ts
@@ -4,11 +4,16 @@ import { fetchFromTMDB } from '@/utils/tmdb';
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   const id = params.id;
   try {
-    const data = await fetchFromTMDB({
-      path: `/tv/${id}`,
-      params: { append_to_response: 'videos,credits' },
-    });
-    return NextResponse.json(data);
+    const [tvShowDetails, similarTVShows] = await Promise.all([
+      fetchFromTMDB({
+        path: `/tv/${id}`,
+        params: { append_to_response: 'videos,credits' },
+      }),
+      fetchFromTMDB({
+        path: `/tv/${id}/similar`,
+      }),
+    ]);
+    return NextResponse.json({ ...tvShowDetails, similarTVShows: similarTVShows.results });
   } catch (error) {
     console.error(`Error fetching tv details for id ${id}:`, error);
     return NextResponse.json({ error: 'Failed to fetch tv details' }, { status: 500 });
