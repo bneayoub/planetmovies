0406bb7ba079bab6e5aedc82f1f52425e4d080c7
bneayoub <bne.ayoub@gmail.com>
2024-07-10 10:29:43 +0100

    chore: Update package dependencies for next-themes and @clerk/themes, and updated middleware for clerk
    This commit updates the package dependencies in package.json and package-lock.json to include the latest versions of next-themes (0.3.0) and @clerk/themes (2.1.10). These updates introduce new features and improvements, enhancing the functionality and performance of the application.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const publicPaths = ["/", "/api(.*)"];
+
+export default clerkMiddleware((auth, req, evt) => {
+  const url = new URL(req.url);
+  if (publicPaths.some(path => url.pathname.match(new RegExp(`^${path}$`)))) {
+    return;
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
