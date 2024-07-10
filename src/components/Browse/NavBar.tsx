71b2ac5e2d0687f68aefe390a78fca0ca87c8b39
bneayoub <bne.ayoub@gmail.com>
2024-07-10 14:25:19 +0100

    refactor: Update navbar layout and logo
    Improve the visual design and user experience by updating the navbar layout and logo. Display the logo as an image within the navbar for a more visually appealing and professional look. Enhance the overall aesthetics and branding of the Cinevault app.

--- src/components/Browse/NavBar.tsx
+++ src/components/Browse/NavBar.tsx
@@ -5,22 +5,22 @@ import { Search } from 'lucide-react';
 
 const BrowseNavbar: React.FC = () => {
   return (
-    <nav className="bg-skin-fill text-skin-base p-4">
+    <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="flex items-center">
+        <img
+            src="/logos/logo.png"
+            alt="Cinevault Logo"
+            className="max-h-40 max-w-40 mr-2"
+            style={{ height: 'auto', width: 'auto' }}
+          />
+        </Link>
         <div className="flex space-x-4">
           <Link href="/browse/movie" className="hover:text-gray-300">Movies</Link>
           <Link href="/browse/tv" className="hover:text-gray-300">TV Shows</Link>
-          <Link href="/watchlist" className="hover:text-gray-300">Watchlist</Link>
+          <Link href="/browse/watchlist" className="hover:text-gray-300">Watchlist</Link>
         </div>
         <div className="flex items-center space-x-4">
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="bg-skin-fill text-skin-base px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-          </div>
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
