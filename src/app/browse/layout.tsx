4dfe0b786e158e09063e6d91b2d04acf0adfaca3
bneayoub <bne.ayoub@gmail.com>
2024-07-13 14:49:50 +0100

    refactor: Update BrowseLayout component to use full width for main content

--- src/app/browse/layout.tsx
+++ src/app/browse/layout.tsx
@@ -10,7 +10,7 @@ export default function BrowseLayout({
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
       <BrowseNavbar />
-      <main className="container mx-auto px-4 py-8">
+      <main className="w-full">
         {children}
       </main>
     </div>
